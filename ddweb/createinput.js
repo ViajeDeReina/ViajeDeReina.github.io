dbee2f18cf0f157ebd4a63fb6409c32809ae900b
Yeahwon Do <viaje.de.reina@kakao.com>
2020-10-07 14:04:40 +0900

    date & time >> date only
    we don't need time in detail actually :b

--- ddweb/createinput.js
+++ ddweb/createinput.js
@@ -2,10 +2,7 @@ const d = new Date();
 const year = d.getFullYear();
 const mon = d.getMonth() + 1;
 const day = d.getDate();
-const hour = d.getHours();
-const mins = d.getMinutes();
-const secs = d.getSeconds();
 
-const fechatiempo = `${year}-${`${mon < 10 ? `0${mon}` : mon}-${day < 10 ? `0${day}` : day} ${hour < 10 ? `0${hour}` : hour}:${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`}`;
+const fechatiempo = year + "-" + `${mon<10 ? `0${mon}`:mon}-${day<10 ? `0${day}`:day}`;
 
 document.getElementById("registeredDate").value = fechatiempo;
